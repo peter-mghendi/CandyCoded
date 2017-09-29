12a614d9901b4c0e5b0c8b861bad27c0acda5f0e
sarah <sarah-holderness@pluralsight.com>
2017-09-29 13:08:50 -0400

    Cleaned up Task 3 tests.

--- CandyCoded/app/src/test/java/com/codeschool/candycoded/Task3.java
+++ CandyCoded/app/src/test/java/com/codeschool/candycoded/Task3.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import junit.framework.Assert;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,8 +15,15 @@ import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
-import static org.junit.Assert.assertEquals;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -25,6 +34,7 @@ import static org.mockito.Mockito.verify;
 @RunWith(PowerMockRunner.class)
 public class Task3 {
 
+    public static final String LAYOUT_XML_FILE = "res/layout/activity_info.xml";
     private static InfoActivity infoActivity;
     private static boolean created_intent = false;
     private static boolean set_data = false;
@@ -47,75 +57,99 @@ public class Task3 {
 
             PowerMockito.whenNew(Intent.class).withAnyArguments().thenReturn(intent);
 
-
             try {
                 infoActivity.onCreate(bundle);
             } catch (Exception e) {
-                e.printStackTrace();
+                //e.printStackTrace();
             }
 
             PowerMockito.mockStatic(Uri.class);
             PowerMockito.when(Uri.class, "parse", "tel:0123456789").thenReturn(mockUri);
             try {
-                infoActivity.createPhoneIntent(null);
+                //infoActivity.createPhoneIntent(null);
+                Method myMethod =  InfoActivity.class
+                        .getMethod("createPhoneIntent", View.class);
+                Object[] param = {null};
+                myMethod.invoke(infoActivity, param);
             } catch (Throwable e) {
-                e.printStackTrace();
+                //e.printStackTrace();
             }
-            PowerMockito.verifyStatic(Uri.class);
-            Uri.parse("tel:0123456789");
-            called_uri_parse = true;
-
             PowerMockito.verifyNew(Intent.class, Mockito.atLeastOnce()).
                     withArguments(Mockito.eq(Intent.ACTION_DIAL));
             created_intent = true;
+
+            PowerMockito.verifyStatic(Uri.class);
+            Uri.parse("tel:0123456789");
+            called_uri_parse = true;
 
             verify(intent).setData(mockUri);
             set_data = true;
 
             Mockito.verify(infoActivity).startActivity(Mockito.eq(intent));
             called_startActivity_correctly = true;
-
-
         } catch (Throwable e) {
-            e.printStackTrace();
+            //e.printStackTrace();
         }
     }
 
     @Test
-    public void t3_1_createPhoneIntent_Exists() throws Exception {
-        Class<?> myClass = null;
+    public void test_combined() throws Exception {
+        createPhoneIntent_Exists();
+        assertTrue("The Intent was not created correctly.", created_intent);
+        assertTrue("The Uri for the phone wasn't created.", called_uri_parse);
+        assertTrue("The data was not set for the Intent.", set_data);
+        assertTrue("The method startActivity() was not called.", called_startActivity_correctly);
+        test_xml();
+    }
+
+    public void createPhoneIntent_Exists() throws Exception {
+        Method myMethod = null;
 
         try {
-            myClass =  InfoActivity.class
-                    .getMethod("createPhoneIntent", View.class)
-                    .getDeclaringClass();
+            myMethod =  InfoActivity.class
+                    .getMethod("createPhoneIntent", View.class);
         } catch (NoSuchMethodException e) {
-            e.printStackTrace();
+            //e.printStackTrace();
         }
 
-        assertEquals(myClass, InfoActivity.class);
+        assertNotNull("createMapIntent() method doesn't exist in InfoActivity class.", myMethod);
     }
 
-    @Test
-    public void t3_2_createIntent() throws Exception {
-        assertTrue(created_intent);
+    public void test_xml() throws Exception {
+        ArrayList<XMLTestHelpers.ViewContainer> viewContainers = readLayoutXML(LAYOUT_XML_FILE);
+        XMLTestHelpers.ViewContainer addressView =
+                new XMLTestHelpers.ViewContainer("@+id/text_view_phone", "createPhoneIntent", "true");
+        boolean address_set_correct =  viewContainers.contains(addressView);
+
+        Assert.assertTrue("In activity_info.xml, the TextView text_view_phone does not have " +
+                        "the clickable and onClick properties set.",
+                address_set_correct);
     }
 
-    @Test
-    public void t3_3_setData() throws Exception {
-        assertTrue(set_data);
+    public ArrayList<XMLTestHelpers.ViewContainer> readLayoutXML(String layoutFileName) {
+        InputStream inputStream = null;
+
+        ArrayList<XMLTestHelpers.ViewContainer> viewContainers = new ArrayList<XMLTestHelpers.ViewContainer>();
+
+        try {
+            inputStream = this.getClass().getClassLoader().getResourceAsStream(layoutFileName);
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(false);
+            XmlPullParser parser = factory.newPullParser();
+            parser.setInput(inputStream, null);
+            parser.nextTag();
+            viewContainers = XMLTestHelpers.readFeed(parser);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return viewContainers;
     }
-
-    @Test
-    public void t3_4_callUriParse() throws Exception {
-        assertTrue(called_uri_parse);
-    }
-
-    @Test
-    public void t3_4_no_startActivity_call() {
-        assertTrue(called_startActivity_correctly);
-    }
-
-
 }
 
